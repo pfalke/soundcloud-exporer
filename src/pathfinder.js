2b813b93159347cd30ff7fe0fb97f383deb05b50
Philipp <pfalkenh@gmail.com>
2013-11-25 18:02:12 -0800

    graph now written separate from algo

--- src/pathfinder.js
+++ src/pathfinder.js
@@ -5,8 +5,9 @@ $(document).ready(function() {
 
 
 	// count how many users have been processed
-	var maxDegree = 2
-	var minNodeDegree = 7
+	var maxDegree = 3
+	var minNodeDegree = 12
+	var minUserDegree = 3
 
 	// parser for halfviz
 	var parse = Parseur().parse
@@ -45,11 +46,6 @@ $(document).ready(function() {
 			var sounds = this.favorites.concat(this.playlisttracks).concat(this.tracks)
 			return sounds
 		}
-
-		// this.setFollowers = function setFollowers(followersList) {
-		// 	this.followers = followersList
-		// 	this.queried.followers = true
-		// }
 	}
 
 	function Sound(id, sound_obj) {
@@ -58,12 +54,70 @@ $(document).ready(function() {
 		this.connectedUsers = [] // users that have favorited etc this sound
 
 		// add a user that has favorited etc this sound to the list
-		this.connectUser = function connectUser(id) {
-			if (this.connectedUsers.indexOf(id) == -1)
-				this.connectedUsers.push(id)
+		this.connectUser = function connectUser(user) {
+			if (this.connectedUsers.indexOf(user) == -1)
+				this.connectedUsers.push(user)
+		}
+
+		// writes source for rendered graph. checks criterion first
+		this.writeEdges = function() {
+			// check if this edge is to appear in the graph
+			if (this.connectedUsers.length<minNodeDegree) return ''
+			// write the output
+			var output = ''
+			var title = this.soundData.title
+			$.each(this.connectedUsers, function(index, user) {
+				output += user.userData.username + ' -> ' + title + '\n'
+			})
+			return output
 		}
 	}
 
+	// writes the source file for the graph and passes it to the parser
+	var writeGraphSource = function() {
+		// check how often each user appers - don't want users that appear only once
+		userCounts = {}
+		function bumpUserCount(index, user) {
+			var id =user.userData.id
+			if (id in userCounts) {
+				userCounts[id] +=1
+			} else {
+				userCounts[id] = 1
+			}
+
+		}
+		// get sounds for the graph
+		soundsInGraph = []
+		for (var soundId in sounds) {
+			if (sounds[soundId].connectedUsers.length>=minNodeDegree) {
+				// this sound is good for graph
+				soundsInGraph.push(sounds[soundId])
+				// bump count for each user associated with sound
+				$.each(sounds[soundId].connectedUsers, bumpUserCount)
+			}
+		}
+
+		console.log(userCounts)
+		// write edges
+		graphSrc = ''
+		$.each(soundsInGraph, function(index, sound) {
+			// node for the sound
+			graphSrc += sound.soundData.title + '\n'
+			// check which users to connect to the node
+			$.each(sound.connectedUsers, function(i, user) {
+				if (userCounts[user.userData.id]>= minUserDegree) {
+					graphSrc += user.userData.username + ' -> ' + sound.soundData.title + '\n'
+					console.log(userCounts[user.userData.id])
+				}
+			})
+		})
+
+		// pass the source to the parser
+		if (graphSrc.length > 0) updateGraph(graphSrc)
+
+		// update again in .5 sec
+		setTimeout(writeGraphSource, 3000)
+	}
 
 	// input is graph in '->' form, send this to halfviz
 	function updateGraph(src_text) {
@@ -75,21 +129,6 @@ $(document).ready(function() {
         _updateTimeout = null
         // display text in input area
         $("#halfviz").find('textarea').val(src_text)
-	}
-
-	// when a sound as at least minNodeDegree edges, include it into graph
-	function checkGraphInclusion(soundId) {
-		var sound = sounds[soundId]
-		if (sound.connectedUsers.length >= minNodeDegree) {
-			console.log('Sound ' + sound.soundData.title + ' into graph!')
-			$.each(sound.connectedUsers, function(index, userId) {
-				// syntax of graph source is "nodename -> nodename"
-				var user = users[userId]
-				console.log(user)
-				edges.push(user.userData.username + ' -> ' + sound.soundData.title)
-				updateGraph(edges.join('\n'))
-			})
-		}
 	}
 
 	// get all favorites, playlist tracks, tracks for a user from soundcloud API
@@ -108,16 +147,16 @@ $(document).ready(function() {
 					sounds[track.id] = new Sound(track.id, track)
 				}
 				// associate user with sounds
-				sounds[track.id].connectUser(id)
+				sounds[track.id].connectUser(user)
 
 				// check new data allows to include the sound into the graph
-				checkGraphInclusion(track.id)
+				// checkGraphInclusion(track.id)
 			}
 
-		user.queried.sounds = true
+			user.queried.sounds = true
 
-		// proceed algorithm with callback
-		if (callback) callback(id, degree)
+			// proceed algorithm with callback
+			if (callback) callback(id, degree)
 			
 		})
 	}
@@ -213,9 +252,42 @@ $(document).ready(function() {
 
 	}
 
+	function startWithOAuth() {
+		// connect to Soundcloud
+		SC.initialize({
+			client_id: SOUNDCLOUD_CLIENT_ID,
+			redirect_uri: SOUNDCLOUD_OAUTH_REDIRECT_URL,
+		});
+		SC.connect(function(){
+			SC.get("/me.json", function(user, error){
+				if(error){
+					console.log("Error: " + error.message)
+				}else{
+					console.log("Start graph search for user " + user.username)
+					rootID = user.id
+					users[rootID] = new User(rootID, user)
+					// start traveling down the tree
+					iterateSounds(rootID,0)
+					// make graph visible
+					$('#path_container').show()
+				}
+			})
+		})
+	}
 
+	function startWithId(id) {
+		$.getJSON('http://api.soundcloud.com/users/'+id+
+		'.json?client_id='+SOUNDCLOUD_CLIENT_ID).done(function(user) {
+			console.log("Start graph search for user " + user.username)
+			rootID = user.id
+			users[rootID] = new User(rootID, user)
+			// start traveling down the tree
+			iterateSounds(rootID,0)
+			// make graph visible
+			$('#path_container').show()
 
-
+		})
+	}
 
 
 
@@ -223,28 +295,16 @@ $(document).ready(function() {
 	// START HERE
 
 	// display "Loading"
-	updateGraph('Loading -> Your Data')
+    var mcp = HalfViz("#halfviz")
+    updateGraph('Loading -> Your Data')
+	setTimeout(function() {	$(window).resize()},300)
+	writeGraphSource()
 
-	// connect to Soundcloud
-	SC.initialize({
-		client_id: SOUNDCLOUD_CLIENT_ID,
-		redirect_uri: SOUNDCLOUD_OAUTH_REDIRECT_URL,
-	});
-	SC.connect(function(){
-		SC.get("/me.json", function(user, error){
-		if(error){
-			console.log("Error: " + error.message);
-		}else{
-			console.log("Start graph search for user " + user.username);
-			rootID = user.id
-			users[rootID] = new User(rootID, user)
-			// start traveling down the tree
-			iterateSounds(rootID,0)
-			// make graph visible
-			$('#path_container').show()
-		}
-		});
-	});
+	// startWithOAuth()
+	startWithId('eleonore-van-roosendaal')
+	// emeli-st-rmer
+	// eleonore-van-roosendaal
+
 })
 
 function checkforduplicates(list) {
