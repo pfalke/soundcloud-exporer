5d5df229d8f494988a06b9db707e0249e474f1ba
Philipp <pfalkenh@gmail.com>
2014-05-11 23:34:55 +0200

    renew SC API IDs

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ import config
 from google.appengine.api import urlfetch
 
 
-
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
